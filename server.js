9b49ec86d9d52a22b5fe97ef8498ae770b5b6d50
amandaneal1131 <amandaneal1131@gmail.com>
2020-12-20 15:59:19 -0600

    s

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
-const apiRoutes = require("./routes/apiRoute.js");
-const htmlRoutes = require("./routes/htmlRoute.js")
+const apiRoutes = require("./routes/apiRoutes.js");
+const htmlRoutes = require("./routes/htmlRoutes.js")
 
 const PORT = process.env.PORT || 3000;
 
